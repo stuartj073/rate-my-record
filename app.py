59ff0343f1e2c20a37a256f8f29ae297133a1ad6
stuartj073 <stuartj073@gmail.com>
2021-10-19 16:36:31 +0000

    Create instance of flask app

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def hello_world():
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
+# Remember to remove debug = True prior to 
+# project submission !!
