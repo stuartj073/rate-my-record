187641040f7fac21625c3e9c26c5b060c015f64e
stuartj073 <stuartj073@gmail.com>
2021-10-20 09:53:51 +0000

    Add app route for log in page

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-def landing_page():
+def landing():
     reviews = mongo.db.reviews.find()
     return render_template("landing_page.html", reviews=reviews)
 
@@ -51,6 +51,33 @@ def register():
         return redirect(url_for('landing'), username=session["user"])
 
     return render_template('register.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
+            else:
+                flash("Incorrect password")
+                return redirect(url_for('login'))
+        
+        else:
+            #username doesn't exist
+            flash("username doesn't exist")
+            return redirect(url_for('login'))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
