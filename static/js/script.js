a609d2228187310538d85d44911b71a8b512888d
stuartj073 <stuartj073@gmail.com>
2021-10-23 21:10:20 +0000

    Add jquery to display different questions on form

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,31 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+
+// $("#music").change(function () {
+//   var form = document.getElementsByTagName('form')
+//   var music = document.getElementById('music')
+//   console.log(music.value)
+//   console.log("HIEIEIF")
+
+//   form.innerHTML=`<h1>Hello</h1>`
+
+// })
+
+
+$("#myselect").on("change", function() {
+  $("#" + $(this).val()).show().siblings().change();
+  console.log("HELLO")
+})
+
+// function changeForm(){
+//   let form = document.getElementsByTagName('form')
+  
+//   if(form.classList.contains("record"){
+//     form.classList.remove("record")
+//   })
+// }
+
+
+
