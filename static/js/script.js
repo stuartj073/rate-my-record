8c3dc9b5abc7260c50ae2d0d0ecbab784133e2e3
stuartj073 <stuartj073@gmail.com>
2021-11-01 13:48:19 +0000

    Add javascript functionality to make delete function more defensive

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,28 @@ $(document).ready(function () {
   $(".dropdown-trigger").dropdown();
 });
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const rules = document.getElementById("rules");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+rules.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
